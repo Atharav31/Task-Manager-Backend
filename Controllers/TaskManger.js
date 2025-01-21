b4ee443763907e54a739f43fc3c91338ba8a3849
Atharav Uttekar <atharavuttekar@gmail.com>
2025-01-21 22:02:18 +0530

    Update and delete

--- Controllers/TaskManger.js
+++ Controllers/TaskManger.js
@@ -3,6 +3,7 @@ import Task from "../Models/Task.js";
 
 export const createTask = async (req, res) => {
   try {
+    console.log(req.body);
     const { title, description, status, userId } = req.body;
     const user = await User.findById(userId);
     if (!user) {
@@ -40,6 +41,40 @@ export const getTask = async (req, res) => {
     const tasks = await Task.find({ userId });
     console.log(tasks);
     res.status(200).json({ message: "Get Task", data: tasks });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const { taskId } = req.params;
+    const { title, description, status } = req.body;
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    task.title = title;
+    task.description = description;
+    task.status = status;
+    await task.save();
+    res.status(200).json({ message: "Task updated successfully", data: task });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const { taskId } = req.params;
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    await task.deleteOne();
+    res.status(200).json({ message: "Task deleted successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
--- Routes/routes.js
+++ Routes/routes.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { login, logout, signup } from "../Controllers/UserController.js";
-import { createTask, getTask } from "../Controllers/TaskManger.js";
+import {
+  createTask,
+  deleteTask,
+  getTask,
+  updateTask,
+} from "../Controllers/TaskManger.js";
 import { isAuthenticated } from "../Middlewares/Cookies.js";
 
 const router = express.Router();
@@ -10,4 +15,6 @@ router.post("/login", login);
 router.get("/logout", logout);
 router.post("/createTask", isAuthenticated, createTask);
 router.get("/getTask/:userId", isAuthenticated, getTask);
+router.put("/updateTask/:taskId", isAuthenticated, updateTask);
+router.delete("/deleteTask/:taskId", isAuthenticated, deleteTask);
 export default router;
