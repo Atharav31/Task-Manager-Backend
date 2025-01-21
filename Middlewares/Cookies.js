09dc6ac9dc50634eb2b8ba725f7ca24cdb456965
Atharav Uttekar <atharavuttekar@gmail.com>
2025-01-21 23:42:51 +0530

    Changes

--- Middlewares/Cookies.js
+++ Middlewares/Cookies.js
@@ -2,30 +2,36 @@ import jwt from "jsonwebtoken";
 import User from "../Models/User.js";
 
 export const setTokenCookie = (res, payload) => {
-  console.log(payload);
   try {
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: "1d",
     });
-    console.log(token);
-    res.cookie("token", token, { httpOnly: true });
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "Strict",
+    });
     return token;
   } catch (error) {
     console.log(error);
   }
 };
+
 export const isAuthenticated = async (req, res, next) => {
-  console.log(req.cookies);
   try {
     const token = req.cookies.token;
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No token provided" });
     }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     const user = await User.findById(decoded.id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
+
     req.user = user;
     next();
   } catch (error) {
